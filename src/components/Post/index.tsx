494fe24a937b871773dd2cfa139cbbf2e1689cc7
maximsmol <maximsmol@gmail.com>
2022-11-20 18:14:12 -0800

    add repost
    Signed-off-by: maximsmol <maximsmol@gmail.com>

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,29 +1,42 @@
-import { JSX } from "solid-js";
+import * as Icon from "lucide-solid";
+import { JSX, Show } from "solid-js";
 
 import styles from "./styles.module.css";
 
 import Avatar from "../Avatar";
+import FlexRow from "../FlexRow/FlexRow";
 
-export default function Post(props: { children: JSX.Element }) {
+export default function Post(props: {
+  repostedBy: string;
+  children: JSX.Element;
+}) {
   return (
     <article class={styles.container}>
-      <Avatar
-        style={{
-          "font-size": "var(--font-size-h2)",
-        }}
-        username="TEST"
-      />
-      <div class={styles.content}>
-        <div class={styles.header}>
-          <span class={styles.name}>hi</span>
-          <span class={styles.headerData}>
-            <span>@test</span>
-            <span>•</span>
-            <span>27m</span>
-          </span>
+      <Show when={props.repostedBy != null}>
+        <div class={styles.repostLine}>
+          <Icon.Repeat />
+          <span>{props.repostedBy} Reposted</span>
         </div>
-        <div>{props.children}</div>
-      </div>
+      </Show>
+      <FlexRow>
+        <Avatar
+          style={{
+            "font-size": "var(--font-size-h2)",
+          }}
+          username="TEST"
+        />
+        <div class={styles.content}>
+          <div class={styles.header}>
+            <span class={styles.name}>hi</span>
+            <span class={styles.headerData}>
+              <span>@test</span>
+              <span>•</span>
+              <span>27m</span>
+            </span>
+          </div>
+          <div>{props.children}</div>
+        </div>
+      </FlexRow>
     </article>
   );
 }
